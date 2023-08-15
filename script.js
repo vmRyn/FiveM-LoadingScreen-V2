a40690ad689b342cbf7ac0042bf8935d7a6823c1
Ryan (Ryn) Dempsey <31045220+vmRyn@users.noreply.github.com>
2023-08-15 21:08:52 +0100

    Script Code Cleanup

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
 const slideshowContainer = document.getElementById('slideshow-container');
+const imgFolder = document.getElementById('img-folder');
 const fullscreenButton = document.getElementById('fullscreen-button');
-let images = []; // Array to hold image URLs
+let images = [];
 let currentIndex = 0;
 let transitioning = false;
-const slideshowInterval = setInterval(showNextImage, 5000); // Change image every 5 seconds
+const slideshowInterval = setInterval(showNextImage, 5000);
+
+function createImageElement(imageUrl, index) {
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.style.position = 'absolute';
+    img.style.top = img.style.left = '0';
+    img.style.opacity = index === currentIndex ? '1' : '0';
+    img.style.objectFit = 'cover';
+    img.style.width = img.style.height = '100%';
+    img.style.zIndex = index === currentIndex ? '1' : '0';
+    return img;
+}
 
 function populateSlideshow() {
-    // Clear existing images
     slideshowContainer.innerHTML = '';
-
-    // Add images from the images array
     images.forEach((imageUrl, index) => {
-        const img = document.createElement('img');
-        img.src = imageUrl;
-        img.style.position = 'absolute';
-        img.style.top = '0';
-        img.style.left = '0';
-        img.style.opacity = index === currentIndex ? '1' : '0';
-        img.style.objectFit = 'cover'; // Ensure the image covers its container
-        img.style.width = '100%';      // Adjust the image width
-        img.style.height = '100%';     // Adjust the image height
-        img.style.zIndex = index === currentIndex ? '1' : '0'; // Set zIndex based on opacity
+        const img = createImageElement(imageUrl, index);
         slideshowContainer.appendChild(img);
     });
 }
 
+function animateTransition(currentImage, nextImage, startTime) {
+    const duration = 1000;
+    const elapsed = performance.now() - startTime;
+    const progress = Math.min(elapsed / duration, 1);
 
-// Set up automatic slideshow
+    currentImage.style.opacity = 1 - progress;
+    nextImage.style.opacity = progress;
+
+    if (progress < 1) {
+        requestAnimationFrame(() => animateTransition(currentImage, nextImage, startTime));
+    } else {
+        transitioning = false;
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+}
+
 function showNextImage() {
     if (transitioning) return;
 
@@ -37,51 +52,20 @@ function showNextImage() {
     const nextImage = imagesList[nextIndex];
 
     if (currentImage && nextImage) {
-        const startTime = performance.now();
-        const duration = 1000; // Transition duration in milliseconds
-
-        function animate(timestamp) {
-            const elapsed = timestamp - startTime;
-            const progress = Math.min(elapsed / duration, 1);
-
-            currentImage.style.opacity = 1 - progress;
-            nextImage.style.opacity = progress;
-
-            if (progress < 1) {
-                requestAnimationFrame(animate);
-            } else {
-                transitioning = false;
-                currentIndex = nextIndex;
-            }
-        }
-
-        requestAnimationFrame(animate);
+        animateTransition(currentImage, nextImage, performance.now());
     }
 }
 
-async function updateImagesAndSlideshow() {
-    const imgFolder = document.getElementById('img-folder');
-    const imgElements = imgFolder.getElementsByTagName('img');
-    
-    // Get the list of all images in the folder
-    const imagesInFolder = Array.from(document.getElementById('img-folder').querySelectorAll('img'));
-    
-    // Compare the images in the folder with the images in the array
+function updateImagesAndSlideshow() {
+    const imagesInFolder = Array.from(imgFolder.querySelectorAll('img'));
     const newImages = imagesInFolder.filter(image => !images.includes(image.src));
-    
-    // Add new images to the images array
+
     if (newImages.length > 0) {
-        images = images.concat(newImages);
+        images.push(...newImages.map(image => image.src));
         console.log('New images added:', newImages);
         populateSlideshow();
     } else {
-        const missingImages = [];
-        for (const image of images) {
-            if (!imagesInFolder.some(img => img.src === image)) {
-                missingImages.push(image);
-            }
-        }
-    
+        const missingImages = images.filter(image => !imagesInFolder.some(img => img.src === image));
         if (missingImages.length > 0) {
             console.log('Missing images:', missingImages);
             // Handle missing images here
@@ -90,10 +74,8 @@ async function updateImagesAndSlideshow() {
 }
 
 async function fetchImagesFromFolder() {
-    // Use fetch or an API library to get image URLs from Discord
-    // Example: const response = await fetch('YOUR_DISCORD_API_URL');
-    // const data = await response.json();
-    // images = data.imageUrls;
+    // Use fetch or an API library to get image URLs
+    // images = await fetch('YOUR_DISCORD_API_URL').then(response => response.json());
 
     // Dummy data for demonstration purposes
     images = [
@@ -106,53 +88,23 @@ async function fetchImagesFromFolder() {
         'img/image7.png',
         'img/image8.png'
     ];
-
-    console.log('Fetched images', images); // Add this line
-
+    
+    console.log('Fetched images:', images);
     populateSlideshow();
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-      }
-      shuffleArray(images);
-      console.log('Shuffled Images:', images); // Output will be a shuffled version of the array
-      populateSlideshow();
+    images.sort(() => Math.random() - 0.5); // Shuffle the images
+    console.log('Shuffled Images:', images);
+    populateSlideshow();
 }
 
-// Fetch images initially
 fetchImagesFromFolder();
 
 function setupImageFolderObserver() {
     const observer = new MutationObserver(updateImagesAndSlideshow);
-
-    // Define the configuration for the observer
     const config = {
-        childList: true, // Watch for changes in the list of child nodes
-        subtree: true,   // Watch for changes in the entire subtree
-        src: true, //Watch for changes to the src attribute of images
+        childList: true,
+        subtree: true,
     };
-
-    // Start observing the 'img' folder
-    observer.observe(document.getElementById('img-folder'), config);
+    observer.observe(imgFolder, config);
 }
 
-// Call the setupImageFolderObserver function
-setupImageFolderObserver();
-
-function setupImageFolderObserver() {
-    const observer = new MutationObserver(updateImagesAndSlideshow);
-
-    // Define the configuration for the observer
-    const config = {
-        childList: true, // Watch for changes in the list of child nodes
-        subtree: true,   // Watch for changes in the entire subtree
-    };
-
-    // Start observing the 'img' folder
-    observer.observe(document.getElementById('img-folder'), config);
-}
-
-// Call the setupImageFolderObserver function
 setupImageFolderObserver();
